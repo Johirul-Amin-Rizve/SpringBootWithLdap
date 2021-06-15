0188c8a56edf02d7fd125af068398fb28dea0ca6
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2021-06-15 12:43:59 +0600

    Initial Commit

---
+++ src/main/java/com/example/SpringBootWithLdap/SpringBootWithLdapApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWithLdap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithLdapApplication.class, args);
+	}
+
+}
